1d05664ab9c348da824c06dab1cc46ad8a717d63
Goldenbwuoy <mumanikidzwag@gmail.com>
2020-08-21 18:19:13 +0800

    Display public stories

--- routes/stories.js
+++ routes/stories.js
@@ -9,6 +9,7 @@ const Story = require('../models/Story')
 router.get('/add', ensureAuth, (req, res) => {
     res.render('stories/add')
 })
+
 //@desc     Process add form
 //@route    POST /stories
 router.post('/', ensureAuth, async (req, res) => {
@@ -16,6 +17,21 @@ router.post('/', ensureAuth, async (req, res) => {
         req.body.user = req.user.id
         await Story.create(req.body)
         res.redirect('/dashboard')
+    } catch (error) {
+        console.log(error)
+        res.render('error/500')
+    }
+})
+
+//@desc     Show all stories
+//@route    GET /stories
+router.get('/', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({status: 'public'})
+            .populate('user')
+            .sort({ createdAt: 'desc'})
+            .lean()
+        res.render('stories/index', { stories })
     } catch (error) {
         console.log(error)
         res.render('error/500')
